2f94c0fd19ea0080e8c08fed665f1d19555d4186
eduardo <eduardo.soares@acct.global>
2021-09-21 21:50:58 -0300

    challenge finished

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,26 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from "./MovieCard"
+import { GenreResponseProps, MovieProps } from "./types"
+import '../styles/content.scss';
+
+interface ContentProps {
+  movieInfo: MovieProps[]
+  selectedGenre: GenreResponseProps
+}
+
+export function Content(props: ContentProps) {
+  return (
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {props.selectedGenre.title}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+        {props.movieInfo.map(movie => (
+              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+        </div>
+      </main>
+  </div>
+  )
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,29 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "./Button"
+import { GenreResponseProps } from "./types"
+import '../styles/sidebar.scss';
+
+
+interface SideBarProps {
+  genreInfo: GenreResponseProps[]
+  handleClickButton: (id: number) => void
+  selectedGenreID: number
+}
+
+export function SideBar(props: SideBarProps) {
+  // function handleClickButton(id: number) {
+  //   setSelectedGenreId(id);
+  // }
+  return (
+    <div className="buttons-container">
+    {props.genreInfo.map(genre => (
+      <Button
+        key={String(genre.id)}
+        title={genre.title}
+        iconName={genre.name}
+        onClick={() => props.handleClickButton(genre.id)}
+        selected={props.selectedGenreID === genre.id}
+      />
+    ))}
+  </div>
+  )
 }
